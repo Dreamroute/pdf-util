0b8940588d69e7b4f7c5227066b1aff39470f56d
w.dehai <wangdehai@bdfint.com>
2020-04-23 16:53:04 +0800

    init.

---
+++ src/main/java/com/github/dreamroute/pdf/util/SpireUtil.java
@@ -0,0 +1,49 @@
+package com.github.dreamroute.pdf.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+import com.github.dreamroute.pdf.util.exception.PdfUtilException;
+import com.spire.pdf.PdfDocument;
+import com.spire.pdf.graphics.PdfImage;
+import com.spire.pdf.widget.PdfPageCollection;
+
+/**
+ * 基于spire.pdf.free框架，免费版只支持10页之内的PDF文件操作
+ * 
+ * @author w.dehai
+ *
+ */
+public class SpireUtil {
+
+    private SpireUtil() {}
+
+    /**
+     * 将一个图片添加到PDF文件上，类似签名
+     * 
+     * @param docInput PDF文件
+     * @param imgInput 图片文件
+     * @param x 相对于左上角x坐标
+     * @param y 相对于左上角y坐标
+     * @return 返回完成后的PDF文件字节数组
+     */
+    public static final byte[] addImgToDoc(InputStream docInput, InputStream imgInput, int x, int y) {
+        PdfDocument doc = new PdfDocument();
+        doc.loadFromStream(docInput);
+        PdfPageCollection pages = doc.getPages();
+        if (pages.getCount() <= 0)
+            throw new PdfUtilException("PDF文件不能为空");
+
+        PdfImage img = PdfImage.fromStream(imgInput);
+        int imgW = img.getWidth();
+        int imgH = img.getHeight();
+
+        // 在最后一页签字
+        pages.get(pages.getCount() - 1).getCanvas().drawImage(img, x, y, imgW, imgH);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        doc.saveToStream(baos);
+        doc.dispose();
+        return baos.toByteArray();
+    }
+
+}
---
+++ src/main/java/com/github/dreamroute/pdf/util/exception/PdfUtilException.java
@@ -0,0 +1,31 @@
+package com.github.dreamroute.pdf.util.exception;
+
+/**
+ * 
+ * @author w.dehai
+ *
+ */
+public class PdfUtilException extends RuntimeException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -8936151616002917662L;
+
+    public PdfUtilException() {
+        super();
+    }
+
+    public PdfUtilException(String message) {
+        super(message);
+    }
+
+    public PdfUtilException(Throwable cause) {
+        super(cause);
+    }
+
+    public PdfUtilException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
