b9730805003bbaa8fdbc36b44ea50a503e44d0ef
w.dehai <w.dehai>
2021-08-09 10:46:22 +0800

    fix: 修改协议，优化单元测试

--- src/test/java/com/github/dreamroute/pdf/util/UtilTest.java
+++ src/test/java/com/github/dreamroute/pdf/util/UtilTest.java
@@ -3,13 +3,12 @@ package com.github.dreamroute.pdf.util;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class UtilTest {
+class UtilTest {
     
     private static final int x = 8;
     private static final int y = 478;
@@ -22,10 +21,10 @@ public class UtilTest {
      * itext
      */
     @Test
-    public void signItextTest() {
-        try (InputStream docIn = new FileInputStream(new File("d:/xxx.pdf"));
-                InputStream imgIn = new FileInputStream(new File("d:/sign.png"));
-                OutputStream out = new FileOutputStream(new File("c:/result1.pdf"))) {
+    void signItextTest() {
+        try (InputStream docIn = new FileInputStream("d:/xxx.pdf");
+             InputStream imgIn = new FileInputStream("d:/sign.png");
+             OutputStream out = new FileOutputStream("d:/result.pdf")) {
             out.write(SignUtil.sign(docIn, imgIn, x, y, imgWidth, imgHeight, rotation,  1));
             Assertions.assertTrue(true);
         } catch (Exception e) {
